62048d6eb705bee128039f2b85429a1162063772
Abhishek Sahani <abhisheksahani653@gmail.com>
2023-08-14 22:09:30 +0530

    Add landing page

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./LandingPage.scss";
+
+function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      <div className="banner-section">
+        {/* <div>
+          <h1 className="bannner-title anime">H</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">i.&nbsp;</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">I'</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">m&nbsp;</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">A</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">b</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">h</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">i</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">s</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">h</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">e</h1>
+        </div>
+
+        <div>
+          <h1 className="bannner-title anime">k</h1>
+        </div> */}
+
+        <div class="container">
+          <div class="box">
+            <div class="title">
+              <span class="block"></span>
+              <h1>
+                Hi. I'm Abhishek.<span></span>
+              </h1>
+            </div>
+
+            <div class="role">
+              <div class="block"></div>
+              <p>A Web developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
