bacd48d5c5653bc06b1ed0f62a7cc60b1f7763a4
Abhishek Sahani <abhisheksahani653@gmail.com>
2023-08-15 19:19:10 +0530

    Add banner section text animation

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -5,66 +5,78 @@ function LandingPage() {
   return (
     <div className="landing-page-container">
       <div className="banner-section">
-        {/* <div>
-          <h1 className="bannner-title anime">H</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">i.&nbsp;</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">I'</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">m&nbsp;</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">A</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">b</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">h</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">i</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">s</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">h</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">e</h1>
-        </div>
-
-        <div>
-          <h1 className="bannner-title anime">k</h1>
-        </div> */}
-
         <div class="container">
           <div class="box">
             <div class="title">
               <span class="block"></span>
-              <h1>
-                Hi. I'm Abhishek.<span></span>
-              </h1>
+
+              <div className="content-wrapper">
+                <div>
+                  <h1 className="bannner-title anime">H</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">i.&nbsp;</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">I'</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">m&nbsp;</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">A</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">b</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">h</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">i</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">s</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">h</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">e</h1>
+                </div>
+
+                <div>
+                  <h1 className="bannner-title anime">k</h1>
+                </div>
+                <span></span>
+              </div>
             </div>
 
             <div class="role">
               <div class="block"></div>
-              <p>A Web developer</p>
+              <div className="description">
+                <div className="flip">
+                  <div>
+                    <div className="field-1" >Web Developer</div>
+                  </div>
+                  <div>
+                    <div className="field-2">Stand-up comedian</div>
+                  </div>
+                  <div>
+                    <div className="field-3">Traveller</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
